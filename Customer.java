71d49953feec419e524509c1efee81e7556dd8ca
Malmii2001 <malmimadhubhashini351002@gmail.com>
2024-07-30 03:21:52 +0530

    first commit

---
+++ Customer.java
@@ -0,0 +1,15 @@
+
+public class Customer {
+
+    private boolean isGold;
+
+    public Customer(boolean isGold) {
+      this.isGold=isGold;
+    }
+
+    public boolean getIsGold() {
+        return isGold;
+        
+    }
+
+}
